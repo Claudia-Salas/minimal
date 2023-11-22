a9da5ed1767d213dd8898086aa2c9dfbed5648fa
Claudia-Salas <141759953+Claudia-Salas@users.noreply.github.com>
2023-11-22 14:44:14 -0600

    Update __init__.py

--- minimal/__init__.py
+++ minimal/__init__.py
@@ -6,3 +6,11 @@ def my_sum(x, y):
 def my_mul(x, y):
     """A function that multiply. """
     return x*y
+ 
+def random (n,R):
+     C=[]
+    for i in range(n):
+        C.append(randint(-R,R))
+    return set(C)
+    
+
